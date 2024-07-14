c3af2dd2277c85c927c0feaa154b8674978eced4
dimple <dsaraogi2000@gmail.com>
2024-07-14 14:45:03 +0545

    add jwt variables to config

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,11 @@ dotenv.config();
 
 const config = {
   port: process.env.PORT,
-  secretKey: process.env.SECRET_KEY,
-  refreshSecretKey: process.env.REFRESH_SECRET_KEY,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    accessExpiration: process.env.JWT_ACCESS_EXPIRATION || "1d",
+    refreshTokenExpiration: process.env.JWT_REFRESH_EXPIRATION || "7d",
+  },
 };
 
 export default config;
