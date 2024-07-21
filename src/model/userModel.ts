42ddd753bf78800d5f6e3ee5141ff398f9148101
Dimple Saraogi <60505670+Dimple278@users.noreply.github.com>
2024-07-21 15:05:08 +0545

    Update userModel to connect to DB

--- src/model/userModel.ts
+++ src/model/userModel.ts
@@ -1,99 +1,6 @@
-
-
-import { User } from "../interface/userInterfaces";
-
-import { readFromFile, writeToFile } from "../utils/fileUtils";
-import ApiError from "../error/apiError";
-import bcrypt from "bcryptjs";
-import notFoundError from "../error/notFoundError";
-import { BaseModel } from "./base";
-
-let users: User[] = readFromFile("users");
-
-export class UserModel extends BaseModel {
-  // static async create(user: User) {
-  //   const userToCreate = {
-  //     name: user.name,
-  //     email: user.email,
-  //     password: user.password,
-  //     role: user.role,
-  //     createdAt: new Date(),
-  //     updatedAt: new Date(),
-  //   };
-
-  //   const [createdUser] = await this.queryBuilder()
-  //     .insert(userToCreate)
-  //     .into("users")
-  //     .returning(["id", "name", "email", "role", "createdAt", "updatedAt"]);
-
-  //   return createdUser;
-  // }
-
-  static async create(user: User) {
-    const userToCreate = {
-      name: user.name,
-      email: user.email,
-      password: user.password,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-
-    const [createdUser] = await this.queryBuilder()
-      .insert(userToCreate)
-      .into("users")
-      .returning(["id", "name", "email", "createdAt", "updatedAt"]);
-
-    return createdUser;
-  }
-  static async update(id: number, user: Partial<User>): Promise<User | null> {
-    const userToUpdate = {
-      ...user,
-      updatedAt: new Date(),
-    };
-
-    await this.queryBuilder().update(userToUpdate).into("users").where({ id });
-
-    const updatedUser = await this.queryBuilder()
-      .select("id", "name", "email", "updatedAt")
-      .from("users")
-      .where({ id })
-      .first();
-
-    return updatedUser || null;
-  }
-
-  static async getUsers(filter: GetUserQuery): Promise<User[]> {
-    const { q } = filter;
-    const query = this.queryBuilder()
-      .select("id", "name", "email")
-      .from("users")
-      .limit(filter.size)
-      .offset((filter.page - 1) * filter.size);
-    if (q) {
-      query.whereLike("name", `%${q}%`);
-    }
-    const users = await query;
-    return users;
-  }
-
-  static count(filter: GetUserQuery) {
-    const { q } = filter;
-    const query = this.queryBuilder().count("*").table("users").first();
-    if (q) {
-      // return users.filter(({name})=>name.includes(q));
-      // query.where({ name: q });
-      query.whereLike("name", `%${q}%`);
-    }
-    return query;
-  }
-}
-export const generateNextUserId = (): number => {
-  const maxId =
-    users.length > 0 ? Math.max(...users.map((user) => user.id)) : 0;
-  return maxId + 1;
-};
-import { Roles } from "../constatnts/Roles";
-import { getUserQuery, User } from "../interface/User";
+import { permissions } from "../constants/Permissions";
+import { Roles } from "../constants/Roles";
+import { getUserQuery, User } from "../interfaces/User";
 import { BaseModel } from "./base";
 
 // User model class
@@ -188,6 +95,7 @@ export class UserModel extends BaseModel {
     }
     return query;
   }
+
   // Function to count users
   static count(filter: getUserQuery) {
     const { q } = filter;
